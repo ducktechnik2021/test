0915303ec9912377814269203bdb6ec1dcdab9ab
黄滔 <huangtao@zalian.cn>
2026-03-20 16:08:58 +0800

    更新cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro:{
-    preset:'vercel',
+    preset:'cloudflare_module',
+    cloudflare:{
+      deployConfig:true,
+      nodeCompat:true
+    }
   }
 })
