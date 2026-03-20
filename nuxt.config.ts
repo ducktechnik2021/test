07d2275aa5ae648cd59328cc24e5be43a36b95d1
黄滔 <huangtao@zalian.cn>
2026-03-20 10:23:06 +0800

    修改nuxt配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  routeRules:{
+    '/':{prerender:true}
+  },
   nitro:{
     preset:'node-server',
     esbuild:{
